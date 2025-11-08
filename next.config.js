ea50030e95a4ff40d1b6834f0f532268085fa6c4
heavybag555 <buribe@inside.artcenter.edu>
2025-11-08 12:45:31 -0800

    repo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['cdn.sanity.io'],
   },
+  // If your GitHub repo is username.github.io, set basePath to ''
+  // If your GitHub repo is username/repo-name, set basePath to '/repo-name'
+  basePath: process.env.NODE_ENV === 'production' ? '/alec-hinson' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/alec-hinson' : '',
 }
 
 module.exports = nextConfig
